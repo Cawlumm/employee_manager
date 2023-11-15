08129b4dbdeadd92a70638ea84560077827db43f
Carter Lumm <cwlumm@gmail.com>
2023-11-15 17:52:23 -0500

    dummy commit

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,7 @@ const app = express();
 
 /*==================================== MIDDLEWARE =========================================*/
 
+
 app.use(cors({
     origin: 'http://localhost:8000',
     credentials: true
